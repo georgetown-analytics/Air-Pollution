56907f42bb52538c125a8e9eae4790cf65e06c8d
julien collaer <collaer@gmail.com>
2020-02-02 08:06:26 -0500

    static server

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -43,3 +43,9 @@ class Measure(db.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="Ground stations in db")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"],
+                               filename)
